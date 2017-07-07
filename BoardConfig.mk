0e4938f676ecf5aa47e497f2bb4941089771ef4b
XiNGRZ <chenxingyu92@gmail.com>
2017-07-08 04:19:29 +0800

    HAX: op5: prebuilt libril.so
    I don't know why the <telephony/ril.h> in TARGET_SPECIFIC_HEADER_PATH
    doesn't override the original one. This leads a wrong libril.so that
    crashes the rild in our nightly build server, but right in my own Mac.
    
    Just use a prebuilt working copy of the file, until we find what's
    wrong.
    
    Change-Id: I6577a6018a0e85018f43bcf6050b4013e5fc0477

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,6 +182,7 @@ BOARD_SUPPRESS_EMMC_WIPE := true
 # RIL
 TARGET_RIL_VARIANT := caf
 PROTOBUF_SUPPORTED := true
+BOARD_PROVIDES_LIBRIL := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
